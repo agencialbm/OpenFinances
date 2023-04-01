c7a3a0967d196fe71ecedde1d7a147bad045908d
Rodrigo Henrique <rodrigo_pbro@hotmail.com>
2023-04-01 18:19:19 -0300

    button formulario

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,22 +1,31 @@
-import { Button } from '@/components/Button';
+import { useState } from 'react';
 import { Footer } from '@/components/Footer';
 import { Header } from '@/components/Header';
-import { Icon } from '@/components/Icon';
 import { TextCard } from '@/components/TextCard';
-import { Content, SectionHeader, ContentTexto, SectionMain, Formulario } from './styled';
+import {
+  Content,
+  SectionHeader,
+  ContentTexto,
+  SectionMain,
+  Formulario,
+  SectionForm
+} from './styled';
 import imgfundo from '../../assets/FundoHeader.png';
 import { MultiplosCard } from '@/components/MultiplosCard';
-import {  FormPJ } from '@/components/FormPJ';
+
 import { FormPF } from '@/components/FormPF';
 
-
 export function Home() {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleActive = () => {
+    setIsActive(true);
+
+  };
+  console.log(isActive);
+
   return (
     <>
-      <Formulario>
-        <FormPF/>
-        <FormPJ/>
-      </Formulario>
       <SectionHeader>
         <Header />
         <Content>
@@ -31,16 +40,39 @@ export function Home() {
           </div>
         </Content>
         <ContentTexto>
-          <TextCard title='Missão' description='Nossa missão é atuar na indicação dos melhores players financeiros do mercado para os nossos clientes por meio de operações customizadas.'/>
-          <TextCard title='Visão' description='Ter uma visão responsável e holística do negócio e do mercado de atuação dos nossos clientes, valorizando a melhor experiência.'/>
-          <TextCard title='Valores' description='Construir relacionamentos duradouros e transparentes, buscando a melhor alternativa de negócios para os nossos clientes.'/>
+          <TextCard
+            title="Missão"
+            description="Nossa missão é atuar na indicação dos melhores players financeiros do mercado para os nossos clientes por meio de operações customizadas."
+          />
+          <TextCard
+            title="Visão"
+            description="Ter uma visão responsável e holística do negócio e do mercado de atuação dos nossos clientes, valorizando a melhor experiência."
+          />
+          <TextCard
+            title="Valores"
+            description="Construir relacionamentos duradouros e transparentes, buscando a melhor alternativa de negócios para os nossos clientes."
+          />
         </ContentTexto>
       </SectionHeader>
       <SectionMain>
         <h2>Conheça nossas soluções de crédito para você e sua empresa</h2>
-        <MultiplosCard/>
+        <MultiplosCard />
       </SectionMain>
-      <Footer/>
+      <SectionForm>
+        <Formulario>
+          <FormPF />
+          <div className="button">
+            <button>Pessoa Física</button>
+            <button
+
+              onClick={handleActive}
+            >
+              Pessoa Jurídica
+            </button>
+          </div>
+        </Formulario>
+      </SectionForm>
+      <Footer />
     </>
   );
 }
--- src/pages/Home/styled.ts
+++ src/pages/Home/styled.ts
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 
-
+interface Prop {
+  isActive: boolean;
+}
 
 export const SectionHeader = styled.div`
 
@@ -78,12 +80,80 @@ padding: 206px 0 175px 0;
 
 `;
 
-export const Formulario = styled.div`
+export const SectionForm = styled.div`
 background: black;
 width: 100%;
-height: 751px;
+padding: 90px 0;
+
+
+`;
+
+export const Formulario = styled.div<Prop>`
+
+background: black;
+width: 100%;
+max-width: 1000px;
 display: flex;
-gap: 30px;
+justify-content: space-between;
+margin: 0 auto;
+
+>.button{
+  display: flex;
+  flex-direction: column;
+  gap: 90px;
+
+  >button{
+
+      font-weight: 600;
+      font-size: 22px;
+      line-height: 27px;
+      position: relative;
+      background: transparent;
+
+      &:after{
+      content: '';
+      width: 6px;
+      height: 42px;
+      position: absolute;
+      background: #FFFFFF;
+      border-radius: 3px;
+      left: 46px;
+
+    }
+
+    { isActive === true ?  : ''}
+    &.isActive{
+      background: #FFFFFF;
+      width: 361px;
+      height: 102px;
+      box-shadow: 0px 0px 15px #FFFFFF;
+      border-radius: 10px;
+      color: black;
+
+      :after {
+        width: 0;
+      }
+    }
+
+    &.isDisabled{
+      background: transparent;
+
+
+      &:after{
+      content: '';
+      width: 6px;
+      height: 42px;
+      position: absolute;
+      background: #FFFFFF;
+      border-radius: 3px;
+      left: 46px;
+
+    }
+    }
+
+  }
+}
+
 
 `;
 
